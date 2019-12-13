bf1c261a85f48fdcdb4f10e2879c02cb5a2bec24
lml <lml256@163.com>
2019-12-13 14:09:13 +0800

    feat(boot): boot中清屏函数，显示字符串函数

--- arch/i386/boot/boot.asm
+++ arch/i386/boot/boot.asm
@@ -1,26 +1,94 @@
 ; mbr引导分区
 
+
+
 	org 7c00h		; 指示程序加载到7c00h处
+
+BaseOfStack	equ	7c00h	; 栈的段地址
+BaseOfScreen	equ	0b800h	; 显存的段地址
+RowOfScreen	equ	25	; 屏幕行数
+ColOfScreen	equ	80	; 屏幕列数
+
+	jmp short _start	; 占用两个字节
+	nop			; 为了满足fat12磁盘的格式，使用nop填充
+
+	; 其中包括fat12磁盘头以及计算后的各部分的偏移位置
+	%include "fat12header.inc"
+
+_start:
+	; 初始化栈和寄存器
 	mov 	ax, cs
 	mov	ds, ax
 	mov 	es, ax
+	mov	ss, ax
+	mov	sp, BaseOfStack
+	mov	ax, BaseOfScreen
+	mov	gs, ax
+
+	call	clear_screen
+	mov	ax, 0302h
+	mov	cx, BootMessage
+
 	call	disp_str
 	hlt			; 停机
 
-disp_str:
-	mov	ax, BootMessage
-	mov	bp, ax		; es:bp 指向字符串首地址
-	mov	cx, [BootMessageLen]
-	mov	ax, 1301h	; ah = 13h号功能  al=01h，目标字符串只包含字符，移动光标
-	mov	bx, 0002h	; bl = 02h 指定颜色属性，黑底绿字
-	mov	dx, 0000h	; 显示在0行0列
-	int	10h
+
+; 清屏
+; 参数：无参数
+clear_screen:
+	push	cx
+	push	di
+	mov	cx, 1999
+	mov	di, 0
+clear_screen_loop:
+	mov	byte [gs:si], 0x20  	; 写空格
+	inc	si
+	mov	byte [gs:si], 0x02	; 黑底绿字
+	inc	si
+	loop	clear_screen_loop
+	pop	di
+	pop	cx
 	ret
 
+
+; 在屏幕上显示字符串
+; 参数： ah=屏幕第几行 al=屏幕第几列 cx=字符串首地址
+disp_str:
+	; 暂存寄存器
+	push 	si
+	push 	di
+
+	; 算出屏幕输出的偏移地址
+	push	ax
+	mov	ah, al
+	mov	ah, ColOfScreen
+	add	ah, ah
+	mul	ah
+	mov	di, ax
+	pop	ax
+	xor	ah, ah
+	add	di, ax
+	add	di, ax
+
+	mov 	si, cx
+
+_disp:	mov 	al, [ds:si]
+	cmp	al, 0
+	jz	_disp_end
+	mov 	[gs:di], al
+	inc	di
+	mov	byte [gs:di], 0x02	; 黑底绿字
+	inc	di
+	inc	si
+	jmp	_disp
+_disp_end:
+	pop 	di
+	pop	si
+	ret
+
+; 字符串统一以0结尾表示结束
 BootMessage:
-	db 'Hello world'
-BootMessageLen:
-	dw (BootMessageLen - BootMessage)
+	db 'Booting....',0x0
 
 times	510-($-$$) db 0
 	dw	0xaa55
